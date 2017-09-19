8143f6742c94580d2531d69bd773ad3e521ef99c
Baruwa fadhlurahman Ayoku <31153941+baruwa123@users.noreply.github.com>
2017-09-19 12:25:33 +0100

    Update email.php

--- profile/baruwa profile/email.php
+++ profile/baruwa profile/email.php
@@ -1,27 +1,19 @@
-<?php if (isset($_GET['send']))
-
-{
-
-         //Email information here
-
-     $to = "olakes265@gmail.com"
-
-     $subject = $_GET['subject'];
-
-     $body = $_GET['message'];    
-
-     $config = include(..'/config.php');
-
-     $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-
-     $con = new PDO($dsn, $config['username'], $config['pass']);
-
-     $exe = $con->query('SELECT * FROM password LIMIT 1');
-
-     $data = $exe->fetch();
-
-     $password = $data['password'];
-
-           header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=olakes265@gmail.com");
-          }
-?>
+<?php
+    $config = include('../config.php');
+    $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+    $con = new PDO($dsn, $config['username'], $config['pass']);
+    $exe = $con->query('SELECT * FROM password LIMIT 1');
+    $data = $exe->fetch();
+    $password = $data['password'];
+    if (isset($_GET['sendmessage'])) {
+        $subject = "Hello";
+        $password = htmlentities(strip_tags(trim($password)));
+        $body = htmlentities(strip_tags(trim($_GET['body'])));
+        $to = "olakes265@gmail.com";
+        if (!isset($body) || $body == '' || (!isset($_GET['name'])) || $_GET['name'] == '' ) {
+            echo "Form cannot be blank";
+        }else {
+            $location = "../sendmail.php?to=$to&subject=$subject&password=$password&body=$body";
+            header("Location: " . $location);
+        }
+    }
