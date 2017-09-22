68cf121682f9977f240e1256d60018a30d0a71cc
flekumxzy <flekumxzy@gmail.com>
2017-09-22 16:29:16 +0100

    k

--- profile/nseabasi.php
+++ profile/nseabasi.php
@@ -1,6 +1,6 @@
 <?p>
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $error = [];
+    $error = array();
     $subject = $_POST['subject'];
     $to  = 'nseabasiokwong@yahoo.com';
     $body = $_POST['commentbox'];
@@ -12,7 +12,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $error[] = 'Your name and email are important to me, TYPE them in';
     }
     if(empty($error)) {
-        $config = include(dirname(dirname(dirname(__FILE__))).'/config.php');
+        $config = include('../../config.php');
         $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
         $con = new PDO($dsn, $config['username'], $config['pass']);
         $exe = $con->query('SELECT * FROM password LIMIT 1');
