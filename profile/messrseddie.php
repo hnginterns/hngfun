b4b02ba5ecbd287c3d7fa005efa81f091acc200f
messrsedddie <31695544+messrsedddie@users.noreply.github.com>
2017-09-23 02:21:43 +0100

    Update messrseddie.php

--- profile/messrseddie.php
+++ profile/messrseddie.php
@@ -1,18 +1,32 @@
 <?php
-//Connect to database
-  $config = include('../../config.php');
-  $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-  $con = new PDO($dsn, $config['username'], $config['pass']);
-  $exe = $con->query('SELECT * FROM password LIMIT 1');
-  $data = $exe->fetch();
-  $password = $data['password'];
-?>
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $error = [];
+    $subject = $_POST['subject'];
+    $to  = 'messrseddie@gmail.com';
+    $body = $_POST['body'];
+    if($body == '' || $body == ' ') {
+      $error[] = "Don't be shy. Write me a message";
+    }
+    if($subject == '' || $subject == ' ') {
+      $error[] = 'A subject would be awesome.';
+    }
+    if(empty($error)) {
+      $config = include __DIR__ . "/../config.php";
+      $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+      $con = new PDO($dsn, $config['username'], $config['pass']);
+      $exe = $con->query('SELECT * FROM password LIMIT 1');
+      $data = $exe->fetch();
+      $password = $data['password'];
+      $url = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
+      header("location: $url");
+    }
+  }
+ ?>
 
  <!-- after php summon -->
 
-
-<!DOCTYPE html> 
-     <html>
+<!DOCTYPE> 
+<html>  
 <head>
      <meta charset ="Utf-8">
 <title>HNG INTERNSHIP 2017</title>
