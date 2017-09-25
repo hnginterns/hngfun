c50824b37a024535728ceab527e34674fb09ab5c
ikem-legend <ikemoobioha@gmail.com>
2017-09-25 19:13:29 +0100

    Readded JS file

--- bt/wordcount.js
+++ bt/wordcount.js
@@ -2,4 +2,15 @@ function wordCount() {
 	document.getElementById('word').onkeyup = function () {
 	document.getElementById('result').innerHTML = "Characters: " +  this.value.length;
 	};
+
 }
+
+// function wordCount() {
+// 	var count = "";
+// 	count = document.getElementById('word').value;
+// 	var letterCount = count.length;
+// 	results = document.getElementById('result');
+// 	results.innerHTML = "<p>You have " + letterCount + " letters</p>";
+// }
+
+// wordCount(count);
