5865e294e795a825c17bac2caa2ef98fc3430e32
ekeminiotong <31893249+ekeminiotong@users.noreply.github.com>
2017-09-26 05:50:10 -0700

    Update ekeminiotong.php

--- profile/ekeminiotong.php
+++ profile/ekeminiotong.php
@@ -84,7 +84,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
           <br>
           <textarea rows="5" cols="40" name="body" placeholder="Write your message/comment here..."></textarea><br><br>
 
-          <submit>Send</submit>
+          <button type="submit">Send</button>
 
         </form>
       </div>
