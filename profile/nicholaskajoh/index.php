32636713cc1b3ce58e5b6e6dd794b57cac551e68
Nicholas Kajoh <kajohterna@gmail.com>
2017-09-27 04:44:49 +0100

    Comment out style.css link.

--- profile/nicholaskajoh/index.php
+++ profile/nicholaskajoh/index.php
@@ -20,7 +20,7 @@
   <title>Nicholas Kajoh | Hotels.ng Internship</title>
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <!--<link rel="stylesheet" type="text/css" href="style.css">-->
 </head>
 <body>
   <!-- navbar -->
