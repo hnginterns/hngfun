19f70e65351a5126e6815b95e4c5815a62a8624a
Victor Dominic <Victor Dominic>
2017-10-08 15:29:30 +0100

    Team Ace

---
+++ Politics/4.php
@@ -0,0 +1,83 @@
+<?php
+	session_start();
+
+	if(isset($_POST["btn_yes"]))
+		{
+				++$_SESSION['id'];
+				
+			}
+
+		else if(isset($_POST["btn_no"])){
+
+				++$_SESSION['num'];
+
+			}
+
+	else{
+		"Error Performing Operation";
+	} 
+ ?>
+
+ <!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+
+		.space{
+			margin-left:200px;
+			margin-top: 85px;
+		}
+
+		.vim{
+
+			width: 50%;
+			height: 400px;
+			background-color: rgb(254,198,58);
+			margin-left: 355px;
+			margin-top: 150px;
+		}
+
+		.vim2{
+			width: 75%;
+			height: 200px;
+			background-color: azure;
+			margin-left: 100px;
+			margin-top: 20px;
+			float:left;
+		}
+
+		h1{
+			color: white;
+			padding-top: 30px;
+			margin-left: 150px;
+		}
+		h3{
+			margin-left: 280px;
+			font-family: oswald;
+		}
+
+		h4{
+			margin-left: 30px;
+			margin-top: 30px;
+		}
+	</style>
+</head>
+<body>
+
+
+	 <div class="vim">
+		<form class="form_back" action="5.php" method="Post">
+			<h1>YOU CHOOSE<sup>&reg</sup></h1>
+			<h3>2019 Nigeria Election</h3>
+
+			<div class="vim2">
+
+		 <h4>4. Create atleast one Functional Airport in each state.</h4> <br>
+		<a href="5.php" ><input class="space" type="submit" name="btn_yes" value="Agree"></a>
+		<a href="5.php" ><input type="submit" name="btn_no" value="I Disagree"></a><br><br>
+
+		</div>
+	</form>
+
+</body>
+</html> 
